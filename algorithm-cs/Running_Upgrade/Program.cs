d7452ebb2a05762eea6882002b9f3b2d7e9d126d
ANT <bit0316@naver.com>
2022-01-11 21:21:19 +0900

    abc

---
+++ algorithm-cs/Running_Upgrade/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+namespace Running_Upgrade
+{
+
+    internal class Program
+    {
+        const string LINE = "-------------------------------";
+        const int LENGTH = 30;
+        static bool goal = false;
+        static int[] run = new int[4] { 0, 0, 0, 0 };
+
+        static void ClearScreen()
+        {
+            Thread.Sleep(100);
+            Console.Clear();
+        }
+
+        static void Process(ref Random rand)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                run[i] += rand.Next(1, 3);  // 0.1초 달린 거리
+                for (int j = 0; j < run[i]; j++) // 달린 거리
+                    Console.Write(" ");
+                Console.Write(i + 1);  // 선수 위치
+                for (int k = LENGTH - run[i] - 1; k > 0; k--)  // 남은 거리
+                    Console.Write(" ");
+                Console.WriteLine("|");  // 결승선
+
+                if (run[i] >= LENGTH)  // 도착 검사
+                    goal = true;
+            }
+
+        }
+
+        static void UpdateScreen()
+        {
+            for (int i = 0; i < 4; i++)
+                if (run[i] >= LENGTH)
+                    Console.WriteLine("Winner : " + (i + 1));
+        }
+
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            while (!goal)
+            {
+                ClearScreen();
+
+                Console.WriteLine(LINE);
+                Process(ref rand);
+                Console.WriteLine(LINE);
+            }
+            UpdateScreen();
+        }
+    }
+
+}   
